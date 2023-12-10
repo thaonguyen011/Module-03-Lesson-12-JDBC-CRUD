2118065b21af058753299c70e78165c513b92bec
thaonguyen011 <thanhthaohoa9999@gmail.com>
2023-12-11 06:48:20 +0700

    update user_management file

--- src/main/java/com/example/user_manager/controller/UserServlet.java
+++ src/main/java/com/example/user_manager/controller/UserServlet.java
@@ -1,5 +1,6 @@
 package com.example.user_manager.controller;
 
+import com.example.user_manager.model.Employee;
 import com.example.user_manager.model.IUserDAO;
 import com.example.user_manager.model.User;
 import com.example.user_manager.model.UserDAO;
@@ -11,7 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name="UserServlet", urlPatterns = "/users")
@@ -38,7 +41,6 @@ public class UserServlet extends HttpServlet {
                 case "edit":
                     updateUser(req, resp);
                     break;
-
                 case "search":
                     searchUser(req, resp);
                     break;
@@ -72,6 +74,12 @@ public class UserServlet extends HttpServlet {
                 case "sort":
                     sortUser(req, resp);
                     break;
+                case "test-without-tran":
+                    testWithoutTran(req, resp);
+                    break;
+                case "test-with-tran":
+                    testWithTran(req, resp);
+                    break;
                 default:
                     listUser(req, resp);
                     break;
@@ -79,17 +87,44 @@ public class UserServlet extends HttpServlet {
         } catch (SQLException e) {
             throw new ServletException();
         }
+    }
 
+    private void testWithTran(HttpServletRequest req, HttpServletResponse resp) throws SQLException, ServletException, IOException{
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<p>Hihi</p>");
+        writer.println("</html>");
+        userDao.insertUpdateUseTransaction();
+    }
+
+    private void testWithoutTran(HttpServletRequest req, HttpServletResponse resp) throws SQLException, ServletException, IOException{
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<p>Hello</p>");
+        writer.println("</html>");
+        userDao.insertUpdateWithoutTransaction();
     }
 
     protected void insertUser(HttpServletRequest req, HttpServletResponse resp)
         throws SQLException, IOException, ServletException {
         String name = req.getParameter("name");
         String email = req.getParameter("email");
-        String address = req.getParameter("address");
-        User newUser = new User(name, email, address);
+        String country = req.getParameter("country");
+        User newUser = new User(name, email, country);
 
-        userDao.insertUser(newUser);
+        String add = req.getParameter("add");
+        String edit = req.getParameter("edit");
+        String delete = req.getParameter("delete");
+        String view = req.getParameter("view");
+
+        List<Integer> permissions = new ArrayList<>();
+
+        if (add != null) permissions.add(1);
+        if (edit != null) permissions.add(2);
+        if (delete != null) permissions.add(3);
+        if (view != null) permissions.add(4);
+
+        userDao.addUserTransaction(newUser, permissions);
 
         RequestDispatcher dispatcher = req.getRequestDispatcher("user/create.jsp");
         dispatcher.forward(req, resp);
@@ -128,7 +163,7 @@ public class UserServlet extends HttpServlet {
     protected void showEditForm(HttpServletRequest req, HttpServletResponse resp)
         throws SQLException, IOException, ServletException {
         int id = Integer.parseInt(req.getParameter("id"));
-        User existingUser = userDao.selectUser(id);
+        User existingUser = userDao.getUserById(id);
         req.setAttribute("user", existingUser);
         RequestDispatcher dispatcher = req.getRequestDispatcher("user/edit.jsp");
         dispatcher.forward(req, resp);
--- src/main/java/com/example/user_manager/model/UserDAO.java
+++ src/main/java/com/example/user_manager/model/UserDAO.java
@@ -1,12 +1,14 @@
 package com.example.user_manager.model;
 
-import java.io.IOException;
+
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO implements IUserDAO{
-    private String jdbcURL = "jdbc:mysql://localhost:3306/demo?useSSL=false";
+    private String jdbcURL = "jdbc:mysql://localhost:3306/users";
     private String jdbcUsername = "root";
     private String jdbcPassword = "Modicung2486!";
 
@@ -17,6 +19,16 @@ public class UserDAO implements IUserDAO{
     private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
     private static final String SELECT_USER_BY_COUNTRY = "select * from users where country = ?";
     private static final String SELECT_ALL_USER_BY_NAME = "select * from users order by name";
+    private static final String CREATE_TABLE_EMPLOYEE = "create table employee (" +
+            "id int(3) not null auto_increment," +
+            "name varchar(120) not null," +
+            "salary int(120) not null," +
+            "created_Date datetime," +
+            "primary key(id)" +
+            ");";
+    private static final String DROP_TABLE_EMPLOYEE = "drop table if exists employee;";
+    private static final String INSERT_EMPLOYEE = "insert into employee(name, salary, created_Date) values (?,?,?);";
+    private static final String UPDATE_EMPLOYEE = "update employee set salary = ? where name = ?;";
 
 
     protected Connection getConnection() {
@@ -33,13 +45,11 @@ public class UserDAO implements IUserDAO{
     }
 
     public void insertUser(User user) throws SQLException {
-        System.out.println(INSERT_USERS_SQL);
         try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
             preparedStatement.setString(1, user.getName());
             preparedStatement.setString(2, user.getEmail());
             preparedStatement.setString(3, user.getCountry());
-            System.out.println(preparedStatement);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             printSQLException(e);
@@ -162,5 +172,223 @@ public class UserDAO implements IUserDAO{
             users.add(user);
         }
         return users;
+    }
+
+    @Override
+    public User getUserById(int id) {
+        User user = null;
+        String query = "{call get_user_by_id(?)}";
+
+        try {
+            Connection connection = getConnection();
+            CallableStatement callableStatement = connection.prepareCall(query);
+            callableStatement.setInt(1, id);
+            ResultSet rs = callableStatement.executeQuery();
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                user = new User(id, name, email, country);
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return user;
+    }
+
+    @Override
+    public void insertUserStore(User user) throws SQLException {
+        String sql = "{CALL insert_user(?,?,?)}";
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            CallableStatement statement = connection.prepareCall(sql);
+            connection.setAutoCommit(false);
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            statement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            printSQLException(e);
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                printSQLException(ex);
+            }
+        }
+
+    }
+
+    @Override
+    public void addUserTransaction(User user, List<Integer> permission) {
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        PreparedStatement pstmtAssignment = null;
+        ResultSet rs = null;
+        try {
+            connection = getConnection();
+            connection.setAutoCommit(false);
+            pstmt = connection.prepareStatement(INSERT_USERS_SQL, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getEmail());
+            pstmt.setString(3, user.getCountry());
+            int rowAffected = pstmt.executeUpdate();
+
+            rs = pstmt.getGeneratedKeys();
+            int userId = 0;
+
+            if (rs.next()) {
+                userId = rs.getInt(1);
+            }
+
+            if (rowAffected == 1) {
+                String sqlPivot = "insert into user_permision(user_id, permision_id) values (?,?)";
+                pstmtAssignment = connection.prepareStatement(sqlPivot);
+                for (int permissionId : permission) {
+                    pstmtAssignment.setInt(1, userId);
+                    pstmtAssignment.setInt(2, permissionId);
+                    pstmtAssignment.executeUpdate();
+                }
+                connection.commit();
+            } else {
+                connection.rollback();
+            }
+        } catch (SQLException e) {
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException ex) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+                if (pstmtAssignment != null) pstmtAssignment.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void insertUpdateWithoutTransaction() {
+       try {
+           Connection connection = getConnection();
+           PreparedStatement psDrop = connection.prepareStatement(DROP_TABLE_EMPLOYEE);
+           psDrop.execute();
+
+           PreparedStatement psCreate = connection.prepareStatement(CREATE_TABLE_EMPLOYEE);
+           psCreate.execute();
+
+           PreparedStatement psInsert = connection.prepareStatement(INSERT_EMPLOYEE);
+           psInsert.setString(1, "Quynh");
+           psInsert.setInt(2, 100000000);
+           psInsert.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+           psInsert.executeUpdate();
+
+           psInsert.setString(1, "Ngan");
+           psInsert.setInt(2, 10000000);
+           psInsert.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+           psInsert.executeUpdate();
+
+           PreparedStatement psUpdate = connection.prepareStatement(UPDATE_EMPLOYEE);
+           psUpdate.setString(1, "hello");
+           psUpdate.setString(2,"Quynh");
+           psUpdate.executeUpdate();
+
+       } catch (Exception ex) {
+           ex.printStackTrace();
+       }
+    }
+
+    @Override
+    public void insertUpdateUseTransaction() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            PreparedStatement psInsert = connection.prepareStatement(INSERT_EMPLOYEE);
+            PreparedStatement psUpdate = connection.prepareStatement(UPDATE_EMPLOYEE);
+
+            statement.execute(DROP_TABLE_EMPLOYEE);
+            statement.execute(CREATE_TABLE_EMPLOYEE);
+
+            connection.setAutoCommit(false);
+
+            psInsert.setString(1, "Quynh");
+            psInsert.setInt(2, 1000000);
+            psInsert.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+            psInsert.execute();
+
+            psInsert.setString(1, "Ngan");
+            psInsert.setInt(2, 1000000);
+            psInsert.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+            psInsert.execute();
+
+            psUpdate.setInt(1, 200000);
+            psUpdate.setString(2,"Quynh");
+            psUpdate.execute();
+
+            connection.commit();
+            connection.setAutoCommit(true);
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public List<Employee> selectAllEmployeeUseCallableStatement() {
+        List<Employee> employees = new ArrayList<>();
+        String query = "{call select_all_employee();}";
+
+        try {
+            Connection connection = getConnection();
+            CallableStatement cS = connection.prepareCall(query);
+            ResultSet rs = cS.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int salary = rs.getInt("salary");
+                Timestamp createdDate = rs.getTimestamp("created_Date");
+                Employee employee = new Employee(id, name, salary, createdDate);
+                employees.add(employee);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
+
+    @Override
+    public List<User> selectAllUserUseCallableStatement() {
+        List<User> employees = new ArrayList<>();
+        String query = "{call select_all_user();}";
+
+        try {
+            Connection connection = getConnection();
+            CallableStatement cS = connection.prepareCall(query);
+            ResultSet rs = cS.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                User employee = new User(id, name, email, country);
+                employees.add(employee);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employees;
     }
 }
